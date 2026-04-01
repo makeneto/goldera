a7d40233e8e0b4592c3905e47f8e50575e53f060
Makene Neto <makenedev@gmail.com>
2026-04-01 18:08:24 +0100

    refactor: create ResultSectionProps

--- src/components/ResultSection.tsx
+++ src/components/ResultSection.tsx
@@ -1,39 +1,7 @@
 import { CircleDollarSign, Plus } from "lucide-react"
 import Summary from "./Summary"
 import { formatCurrency } from "../utils/formatCurrency"
-import type { OperationResults } from "../types/OperationResults"
-
-interface ResultSectionProps {
-  goldCost: number
-  amountPaid: number
-  amountPaidFormatted: string
-  isIntermediary: boolean
-  setIsIntermediary: (value: boolean) => void
-  isInvestor: boolean
-  setIsInvestor: (value: boolean) => void
-  investorNames: string[]
-  investorAmountsFormatted: string[]
-  investorsTotal: number
-  onInvestorNameChange: (
-    index: number,
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => void
-  onInvestorAmountChange: (
-    index: number,
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => void
-  onAddInvestor: () => void
-  onInject: () => void
-  productName: string
-  grams: number
-  karats: number
-  investorShares: Array<{ name: string; percentage: number; amount: number }>
-  isInjectSummary: boolean
-  onAmountChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onCalculate: () => void
-  operationResults: OperationResults | null
-  summaryRef: React.RefObject<HTMLDivElement | null>
-}
+import type { ResultSectionProps } from "../types/ResultSectionProps"
 
 export default function ResultSection({
   goldCost,
