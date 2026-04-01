4ce6cbe128195629dd6287d2af45f9b3da24e054
Makene Neto <makenedev@gmail.com>
2026-04-01 15:24:10 +0100

    feat: develop investors section

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,48 +4,58 @@ import ResultSection from "./ResultSection"
 import { useGoldForm } from "../hooks/useGoldForm"
 
 export default function Form() {
-    const {
-        setGrams,
-        karats,
-        setKarats,
-        amountPaid,
-        amountPaidFormatted,
-        isIntermediary,
-        setIsIntermediary,
-        operationResults,
-        summaryRef,
-        goldCost,
-        handleAmountPaidChange,
-        isFormFilled,
-        handleCalculate,
-    } = useGoldForm()
+  const {
+    setGrams,
+    karats,
+    setKarats,
+    amountPaid,
+    amountPaidFormatted,
+    isIntermediary,
+    setIsIntermediary,
+    isInvestor,
+    setIsInvestor,
+    investorNames,
+    investorAmountsFormatted,
+    investorsTotal,
+    handleInvestorNameChange,
+    handleInvestorAmountChange,
+    operationResults,
+    summaryRef,
+    goldCost,
+    handleAmountPaidChange,
+    isFormFilled,
+    handleCalculate,
+  } = useGoldForm()
 
-    return (
-        <div className="form">
-            <div className="form__container">
-                <CustomerFields onGramsChange={setGrams} />
-                <KaratSelector selected={karats} onSelect={setKarats} />
-            </div>
+  return (
+    <div className="form">
+      <div className="form__container">
+        <CustomerFields onGramsChange={setGrams} />
+        <KaratSelector selected={karats} onSelect={setKarats} />
+      </div>
 
-            {!isFormFilled ? (
-                <img
-                    src="/gold-bar.jpg"
-                    className="gold-bars"
-                    alt="Gold Bars"
-                />
-            ) : (
-                <ResultSection
-                    goldCost={goldCost}
-                    amountPaid={amountPaid}
-                    amountPaidFormatted={amountPaidFormatted}
-                    isIntermediary={isIntermediary}
-                    setIsIntermediary={setIsIntermediary}
-                    onAmountChange={handleAmountPaidChange}
-                    onCalculate={handleCalculate}
-                    operationResults={operationResults}
-                    summaryRef={summaryRef}
-                />
-            )}
-        </div>
-    )
+      {!isFormFilled ? (
+        <img src="/gold-bar.jpg" className="gold-bars" alt="Gold Bars" />
+      ) : (
+        <ResultSection
+          goldCost={goldCost}
+          amountPaid={amountPaid}
+          amountPaidFormatted={amountPaidFormatted}
+          isIntermediary={isIntermediary}
+          setIsIntermediary={setIsIntermediary}
+          isInvestor={isInvestor}
+          setIsInvestor={setIsInvestor}
+          investorNames={investorNames}
+          investorAmountsFormatted={investorAmountsFormatted}
+          investorsTotal={investorsTotal}
+          onInvestorNameChange={handleInvestorNameChange}
+          onInvestorAmountChange={handleInvestorAmountChange}
+          onAmountChange={handleAmountPaidChange}
+          onCalculate={handleCalculate}
+          operationResults={operationResults}
+          summaryRef={summaryRef}
+        />
+      )}
+    </div>
+  )
 }
